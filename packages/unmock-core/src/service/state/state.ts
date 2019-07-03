20c9645877ec43cb9ce9c15bdfb15f269f59d6b4
Cain (Meeshkan) <idan@meeshkan.com>
2019-07-03 10:56:02 +0300

    WIP get state method

--- packages/unmock-core/src/service/state/state.ts
+++ packages/unmock-core/src/service/state/state.ts
@@ -1,5 +1,9 @@
 import debug from "debug";
 import {
+  DEFAULT_STATE_ENDPOINT,
+  DEFAULT_STATE_HTTP_METHOD,
+} from "../constants";
+import {
   ExtendedHTTPMethod,
   IResponsesFromOperation,
   Schema,
@@ -8,7 +12,9 @@ import { IOperationForStateUpdate, IStateUpdate } from "./interfaces";
 import { getOperations } from "./utils";
 import { getValidResponsesForOperationWithState } from "./validator";
 
+//           (e.g. "application/json")   schema
 type mediaTypeToSchema = Record<string, Schema>;
+//                              status   (e.g. "application/json")
 type statusToMediaType = Record<string, mediaTypeToSchema>;
 //                              path           method       status
 type ServiceStateType = Record<string, Record<string, statusToMediaType>>;
@@ -65,6 +71,53 @@ export class State {
       throw new Error(opsResult.lastError);
     }
     return;
+  }
+
+  /**
+   * Returns the state for given combination of method, endpoint and status code (if given)
+   * @param method
+   * @param endpoint
+   * @param statusCode
+   */
+  public getState(
+    method: ExtendedHTTPMethod,
+    endpoint: string,
+    statusCode?: string,
+  ): Record<string, Schema> | Schema | undefined {
+    let endpointState: Record<string, statusToMediaType>;
+    let methodState: statusToMediaType;
+    let finalState: mediaTypeToSchema;
+    if (endpoint !== DEFAULT_STATE_ENDPOINT) {
+      if (this.state[endpoint] === undefined) {
+        return undefined;
+      }
+      endpointState = this.state[endpoint];
+    } else {
+      endpointState = {};
+      // all endpoints match, will have to filter by method
+    }
+
+    if (method !== DEFAULT_STATE_HTTP_METHOD) {
+      if (endpointState[method] === undefined) {
+        return undefined;
+      }
+      methodState = endpointState[method];
+    } else {
+      methodState = {};
+      // all methods apply
+    }
+
+    if (statusCode !== undefined) {
+      finalState = methodState[statusCode];
+    } else {
+      // all status codes apply
+      finalState = {};
+    }
+
+    if (Object.keys(finalState).length === 1) {
+      return finalState[Object.keys(finalState)[0]];
+    }
+    return Object.keys(finalState).length > 0 ? finalState : undefined;
   }
 
   private updateStateInternal(
